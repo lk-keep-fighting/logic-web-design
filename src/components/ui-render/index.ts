33557a46f19bcd3259d326957de9984dd5a5a140
lk <442969153@qq.com>
2024-03-11 19:48:04 +0800

    refactor: 优化组件目录结构

---
+++ src/components/ui-render/index.ts
@@ -0,0 +1,8 @@
+import FormRenderById from "./form-render/render-by-form-id";
+import PageRenderById from "./page-render/render-by-page-id";
+import { TokenUtil } from "./utils/tokenUtil";
+export {
+    FormRenderById,
+    PageRenderById,
+    TokenUtil
+}
