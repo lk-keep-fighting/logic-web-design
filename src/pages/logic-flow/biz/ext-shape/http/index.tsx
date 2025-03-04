c90dbc58f39115ce9b101ebf30bea5d4cf17cfb5
刘锟 <liukun@aimstek.cn>
2025-03-04 14:25:29 +0800

    style:js、http

--- src/pages/logic-flow/biz/ext-shape/http/index.tsx
+++ src/pages/logic-flow/biz/ext-shape/http/index.tsx
@@ -5,7 +5,27 @@ import { ExtShapeReactNode } from "../extShape";
 
 export const HttpNode = (props) => {
     const node: Node = props.node;
+    const name = node.data.config?.name;
+    const type = node.data.config?.type;
+    const memo = node.data.config?.memo;
+    const imgSrc = node.prop('imgSrc');
+    const text = node.prop('text');
+    const backgroundColor = node.prop('backgroundColor');
     return (
-        <ExtShapeReactNode node={node} />
+        <Popover content={memo ? <Input.TextArea style={{ width: 300, height: 200 }} readOnly value={memo} ></Input.TextArea> : ''} trigger="click" mouseLeaveDelay={0}>
+            <div
+                className={styles.customNode}
+                style={{
+                    width: '100%', height: '100%', border: '1px solid rgb(20, 145, 235)', borderRadius: '5px', position: 'relative',
+                    overflow: 'hidden',
+                    backgroundColor: backgroundColor?.includes('#') ? backgroundColor : 'white'
+                }}
+            >
+                <img src={imgSrc ? imgSrc : `/logic/icons/${type}.svg`} style={{ width: 20, height: 20, position: 'absolute', top: 0, left: 5, margin: 0 }} />
+                <div style={{ marginTop: 12, whiteSpace: 'pre-wrap', padding: 0, fontSize: '12px', wordWrap: 'break-word', textAlign: 'left', lineHeight: '1.2' }}>
+                    {name || text}
+                </div>
+            </div>
+        </Popover >
     )
 }
