63982c69da6f9e0731b80bc3095a046597a4d2f8
lk <442969153@qq.com>
2025-11-25 17:10:51 +0800

    优化dsl

--- src/components/lib/lasl/meta-data/Logic.ts
+++ src/components/lib/lasl/meta-data/Logic.ts
@@ -35,20 +35,3 @@ export default class Logic {
      */
     visualConfig?: any;
 }
-
-// export interface Logic {
-//     concept: ConceptEnum.Logic,
-//     name: string, // 逻辑名称
-//     label: string, // 逻辑标题
-//     description: string, // 逻辑描述
-//     triggerType: string, // triggerType
-//     cron: string, // cron表达式，用于定时触发
-//     transactional: Transactional, // 事务
-//     compilerInfoMap: { java?: { packageName: string, className: string } }, // 编译器信息，目前仅在后端扩展模块中使用。
-//     typeParams: Array<TypeParam>, // 类型参数列表
-//     params: Array<Param>, // 输入参数列表
-//     returns: Array<Return>, // 输出参数列表
-//     variables: Array<Variable>, // 变量列表
-//     body: Array<LogicItem>, // 逻辑项列表
-//     playground: Array<LogicItem>, // 逻辑项列表
-// }
